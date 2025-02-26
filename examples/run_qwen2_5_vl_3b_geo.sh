3262ef6a3ee05cd0577056d8e94fe9e7b58cd7c6
DVampire <1271421361@qq.com>
2025-02-26 12:11:02 +0800

    init

---
+++ examples/run_qwen2_5_vl_3b_geo.sh
@@ -0,0 +1,15 @@
+set -x
+
+export VLLM_ATTENTION_BACKEND=XFORMERS
+
+MODEL_PATH=Qwen/Qwen2.5-VL-3B-Instruct  # replace it with your local file path
+
+python3 -m verl.trainer.main \
+    config=examples/grpo_example.yaml \
+    data.train_files=hiyouga/geometry3k@train \
+    data.val_files=hiyouga/geometry3k@test \
+    worker.actor.model.model_path=${MODEL_PATH} \
+    worker.rollout.tensor_parallel_size=1 \
+    worker.rollout.enable_chunked_prefill=false \
+    trainer.experiment_name=qwen2_5_vl_3b_geo \
+    trainer.n_gpus_per_node=2
